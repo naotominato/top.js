50c7fcf6947f39dfcc7aa0ff0871fb1116d02f9f
naoto <a_naoto@outlook.jp>
2022-05-18 16:58:25 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function scrollTop(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+
+scrollTop('button');
+
